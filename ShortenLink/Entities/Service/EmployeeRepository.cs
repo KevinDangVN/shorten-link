da8326de5b1fc78dcf05147dd75de9da31268e0d
kevin <dqtkien@gmail.com>
2021-01-04 20:22:25 +0700

    Add employee

---
+++ ShortenLink/Entities/Service/EmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using Entities.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Service
+{
+    class EmployeeRepository
+    {
+        private readonly ShortenLinkContext _context;
+
+        public EmployeeRepository(ShortenLinkContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+    }
+}
