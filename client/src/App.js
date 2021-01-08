3e504144a175aaa535e12534466bd8909d46e54f
kevin <dqtkien@gmail.com>
2021-01-08 22:46:02 +0700

    add

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import Home from "./Home";
 import Slug from "./Slug";
 
 const App = () => {
   const routes = (
     <Switch>
+      <Route path="/" component={Home} exact />
       <Route path="/:slug" component={Slug} />
-
-      <Redirect to="/" />
+      <Redirect to="/" component={Home} />
     </Switch>
   );
 
   return (
     <>
-      abc
       <BrowserRouter>{routes}</BrowserRouter>
     </>
   );
--- client/src/Slug.jsx
+++ client/src/Slug.jsx
@@ -1,5 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 const Slug = () => {
-  return <>slug</>;
+  const [timer, setTimer] = useState(5);
+  const [slugString, setSlugString] = useState("");
+
+  const { slug } = useParams();
+
+  useEffect(() => {
+    const countDown = () => {
+      if (timer === 0) {
+        clearInterval(clock);
+        if (slugString !== "") window.location = slugString;
+      } else setTimer((pre) => pre - 1);
+    };
+
+    const getSlug = async () => {
+      const response = await axios.patch(
+        `http://localhost:5000/api/linkdata/view/${slug}`
+      );
+      console.log(response);
+      setSlugString(response.data.fullLink);
+    };
+    if (slugString === "") getSlug();
+
+    const clock = setInterval(() => {
+      countDown();
+    }, 1000);
+
+    return () => {
+      clearInterval(clock);
+    };
+  }, [timer, slugString, slug]);
+  return <>Your link will be ready in {timer} seconds</>;
 };
 
 export default Slug;
